982e0080fcedf639c52a2ad292a77c9587bf15d0
Zubin Mukherjee <zubin.mukherjee@mindinventory.com>
2021-01-27 18:58:47 +0530

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
+const App = () => {
+    const initialFormState = { id: null, name: '', username: '' }
+    const [currentUser, setCurrentUser] = useState(initialFormState)
+    const updateUser = (id, updatedUser) => {
+        setEditing(false)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        setUsers(users.map((user) => (user.id === id ? updatedUser : user)))
+    }
+    const usersData = []
+    const editRow = (user) => {
+        setEditing(true)
+
+        setCurrentUser({ id: user.id, name: user.name, username: user.username })
+    }
+    const [users, setUsers] = useState(usersData)
+    const addUser = (user) => {
+        user.id = users.length + 1
+        setUsers([...users, user])
+    }
+    const [editing, setEditing] = useState(false)
+
+    const deleteUser = (id) => {
+        setUsers(users.filter((user) => user.id !== id))
+    }
+
+    return (
+            <div className="container">
+                <div className="flex-row">
+                    <div className="flex-large">
+                        {editing ? (
+                            <div>
+                                <h2>Edit user</h2>
+                                <EditUserForm
+                                    setEditing={setEditing}
+                                    currentUser={currentUser}
+                                    updateUser={updateUser}
+                                />
+                            </div>
+                        ) : (
+                            <div>
+                                <h2>Add user</h2>
+                                <AddUserForm addUser={addUser} />
+                            </div>
+                        )}
+                    </div>
+                    <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+                </div>
+            </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/forms/EditUserForm.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+
+const EditUserForm = (props) => {
+    const [user, setUser] = useState(props.currentUser)
+
+    const handleInputChange = (event) => {
+        const {name, value } = event.target
+
+        setUser({...user, [name]: value})
+    }
+
+
+    return (
+        <form onSubmit={(event) => {
+        event.preventDefault()
+        props.updateUser(user.id,user)
+        }}>
+            <lable>Name</lable>
+            <input type="text" name="name" value={user.name} onChange={handleInputChange}/>
+            <lable>Username</lable>
+            <input type="text" name="username" value={user.name} onChange={handleInputChange} />
+            <button>Update User</button>
+            <button onClick={()=> props.setEditing(false)} className="button muted-button">Cancel</button>
+        </form>
+    )
+}
+
+
+export default EditUserForm
